7deecb8d49c7488ea131156682e11f21db0bf8ac
dsavitsky <dsavitsky@evolution.com>
2023-12-02 13:21:16 +0100

    Complete day 2

---
+++ src/main/scala/com/github/desavitsky/day2/Part1.scala
@@ -0,0 +1,75 @@
+package com.github.desavitsky.day2
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Part1:
+
+  object MaxCount:
+    val RedCubes   = 12
+    val GreenCubes = 13
+    val BlueCubes  = 14
+
+  case class Game(
+    id: Int,
+    sets: List[Set[Color]]
+  )
+
+  object Sets:
+
+    def unapply(str: String): Option[List[Set[Color]]] =
+      val strSet = str.split(";").toList
+
+      @tailrec
+      def rev(strSet: List[String])(acc: List[Set[Color]]): Option[List[Set[Color]]] =
+        strSet match
+          case Set(colorSet) :: tail => rev(tail)(colorSet :: acc)
+          case _ :: tail             => None
+          case Nil                   => Some(acc)
+      rev(strSet)(List.empty)
+
+  object Set:
+    def unapply(set: String): Option[Set[Color]] =
+      val splittedSeq = set.split(",").map(_.strip)
+      val colorsCount = splittedSeq.length
+      val colors      = set
+        .split(",")
+        .map(_.strip)
+        .flatMap(Color.unapply)
+      if colors.length == colorsCount then Some(colors.toSet)
+      else None
+
+  object Game:
+    private val gameRegex = "Game ([\\d]+):(.*)".r
+
+    def unapply(game: String): Option[Game] = game match
+      case gameRegex(AsInt(id), Sets(sets)) =>
+        Some(Game(id, sets))
+      case _                                => None
+
+  object AsInt:
+    def unapply(str: String): Option[Int] =
+      str.toIntOption
+
+  sealed trait Color
+
+  object Color:
+    def unapply(str: String): Option[Color] = str match
+      case s"${AsInt(count)} blue" if count <= MaxCount.BlueCubes   => Some(Blue(count))
+      case s"${AsInt(count)} red" if count <= MaxCount.RedCubes     => Some(Red(count))
+      case s"${AsInt(count)} green" if count <= MaxCount.GreenCubes => Some(Green(count))
+      case _                                                        => None
+
+  case class Red(count: Int)   extends Color
+  case class Green(count: Int) extends Color
+  case class Blue(count: Int)  extends Color
+
+@main def run =
+  val result = Source
+    .fromResource("day2/day2.txt")
+    .getLines()
+    .toVector
+    .flatMap(Part1.Game.unapply)
+    .map(_.id)
+    .sum
+  println(result)
---
+++ src/main/scala/com/github/desavitsky/day2/Part2.scala
@@ -0,0 +1,83 @@
+package com.github.desavitsky.day2
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+object Part2:
+
+  case class MinimalGame(
+    id: Int,
+    power: Int
+  )
+
+  object Sets:
+
+    def unapply(str: String): Option[Vector[Color]] =
+      Some(
+        str
+          .split(";")
+          .toVector
+          .flatMap(Set.unapply)
+          .flatten
+      )
+
+  object Set:
+    def unapply(set: String): Option[Set[Color]] =
+      val splittedSeq = set.split(",").map(_.strip)
+      val colorsCount = splittedSeq.length
+      val colors      = set
+        .split(",")
+        .map(_.strip)
+        .flatMap(Color.unapply)
+      if colors.length == colorsCount then Some(colors.toSet)
+      else None
+
+  object MinimalGame:
+    private val gameRegex = "Game ([\\d]+):(.*)".r
+
+    @tailrec
+    def collectFirstColors(colors: List[Color])(acc: Vector[Color]): Vector[Color] =
+      colors match {
+        case head :: tail if acc.size < 3 =>
+          head match
+            case Blue(count) if acc.collectFirst { case Blue(c) => c }.isDefined => collectFirstColors(tail)(acc)
+
+            case Green(count) if acc.collectFirst { case Green(c) => c }.isDefined => collectFirstColors(tail)(acc)
+            case Red(count) if acc.collectFirst { case Red(c) => c }.isDefined     => collectFirstColors(tail)(acc)
+            case notYet                                                            => collectFirstColors(tail)(acc :+ notYet)
+        case _                            => acc
+      }
+
+    def unapply(game: String): Option[MinimalGame] = game match
+      case gameRegex(AsInt(id), Sets(sets)) =>
+        val sorted    = sets.sortBy(-_.count).toList
+        val colorsMin = collectFirstColors(sorted.tail)(Vector(sorted.head))
+        Some(MinimalGame(id, colorsMin.map(_.count).product))
+      case _                                => None
+
+  object AsInt:
+    def unapply(str: String): Option[Int] =
+      str.toIntOption
+
+  sealed trait Color(val count: Int)
+
+  object Color:
+    def unapply(str: String): Option[Color] = str match
+      case s"${AsInt(count)} blue"  => Some(Blue(count))
+      case s"${AsInt(count)} red"   => Some(Red(count))
+      case s"${AsInt(count)} green" => Some(Green(count))
+      case _                        => None
+
+  case class Red(override val count: Int)   extends Color(count)
+  case class Green(override val count: Int) extends Color(count)
+  case class Blue(override val count: Int)  extends Color(count)
+
+@main def run2 =
+  val result = Source
+    .fromResource("day2/day2.txt")
+    .getLines()
+    .toVector
+    .flatMap(Part2.MinimalGame.unapply)
+    .map(_.power)
+    .sum
+  println(result)
