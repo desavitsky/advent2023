42fd3fe31ef654d457a6900cef3af7f5012f5a9e
dsavitsky <dsavitsky@evolution.com>
2023-12-12 20:03:52 +0100

    Day 12 clean-up

--- src/main/scala/com/github/desavitsky/day12/day12.scala
+++ src/main/scala/com/github/desavitsky/day12/day12.scala
@@ -7,58 +7,39 @@ def parseLine(line: String, groups: Vector[Int]): Long =
   val size = line.length
 
   def parseSubString(subString: String)(currentGroup: Int, prevGroups: Vector[Int])(
-    memo: Map[Int, Vector[Vector[Int]]]
-  ): Vector[Vector[Int]] =
-    if memo.contains(subString.length) && currentGroup == 0 then
-      memo
-        .getOrElse(subString.length, Vector.empty)
-        .map(_.prependedAll(prevGroups))
-        .filter(groups.endsWith(_))
-    else if subString.isEmpty then Vector(prevGroups ++ Vector(currentGroup).filter(_ != 0)).filter(groups.endsWith(_))
-    else
-      subString.head match
-        case '.' => parseSubString(subString.tail)(0, prevGroups ++ Vector(currentGroup).filter(_ != 0))(memo)
-        case '#' => parseSubString(subString.tail)(currentGroup + 1, prevGroups)(memo)
-        case '?' =>
-          parseSubString(subString.tail)(0, prevGroups ++ Vector(currentGroup).filter(_ != 0))(memo) ++
-            parseSubString(subString.tail)(currentGroup + 1, prevGroups)(memo)
-
-  def parseSubString2(subString: String)(currentGroup: Int, prevGroups: Vector[Int])(
     memo: Map[Int, Map[Vector[Int], Long]]
   ): Map[Vector[Int], Long] =
     if memo.contains(subString.length) && currentGroup == 0 then
-      val res = memo
+      memo
         .getOrElse(subString.length, Map.empty)
         .map((key, q) => key.prependedAll(prevGroups) -> q)
         .view
         .filterKeys(groups.endsWith(_))
         .toMap
-      res
     else if subString.isEmpty then
       val upd = prevGroups ++ Vector(currentGroup).filter(_ != 0)
-      val res =
-        if groups.endsWith(upd) then Map(upd -> 1L)
-        else Map.empty
-      res
+      if groups.endsWith(upd) then Map(upd -> 1L)
+      else Map.empty
     else
       subString.head match
-        case '.' => parseSubString2(subString.tail)(0, prevGroups ++ Vector(currentGroup).filter(_ != 0))(memo)
-        case '#' => parseSubString2(subString.tail)(currentGroup + 1, prevGroups)(memo)
+        case '.' => parseSubString(subString.tail)(0, prevGroups ++ Vector(currentGroup).filter(_ != 0))(memo)
+        case '#' => parseSubString(subString.tail)(currentGroup + 1, prevGroups)(memo)
         case '?' =>
-          val left  = parseSubString2(subString.tail)(0, prevGroups ++ Vector(currentGroup).filter(_ != 0))(memo)
-          val right = parseSubString2(subString.tail)(currentGroup + 1, prevGroups)(memo)
-          val res   = right ++ left.map((combs, q) => combs -> (right.getOrElse(combs, 0L) + q))
-          res
+          val left  = parseSubString(subString.tail)(0, prevGroups ++ Vector(currentGroup).filter(_ != 0))(memo)
+          val right = parseSubString(subString.tail)(currentGroup + 1, prevGroups)(memo)
+          right ++ left.map((combs, q) => combs -> (right.getOrElse(combs, 0L) + q))
 
-  val res = (1 to size)
+  (1 to size)
     .foldLeft(Map.empty[Int, Map[Vector[Int], Long]]) { case (memo, i) =>
-      val res = parseSubString2(line.takeRight(i))(0, Vector.empty)(memo)
+      val res = parseSubString(line.takeRight(i))(0, Vector.empty)(memo)
       memo.updated(i, res)
     }
+    .getOrElse(size, Map.empty)
+    .filter(_._1 == groups)
+    .values
+    .sum
 
-  res.getOrElse(size, Map.empty).filter(_._1 == groups).values.sum
-
-@main def run2 =
+@main def run =
   val input = Source
     .fromResource("day12/task.txt")
     .getLines()
